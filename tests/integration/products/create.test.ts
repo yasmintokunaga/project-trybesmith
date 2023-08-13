06b2c546c83ba3deec54eed5971ace1ff97f0d0e
yasmintokunaga <yasmintkng@gmail.com>
2023-08-13 19:48:27 -0300

    feat(REQ01): Create the endpoint POST /products

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -2,9 +2,22 @@ import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 
+import createMock from '../../mocks/create.mock';
+import ProductModel from '../../../src/database/models/product.model';
+import productService from '../../../src/services/products.service';
+
+
 chai.use(chaiHttp);
 
 describe('POST /products', function () { 
   beforeEach(function () { sinon.restore(); });
+  describe('testar se é criado um novo produto', async function () {
+    const parameters = createMock.newProductRequest;
+    const mockCreateReturn = ProductModel.build(createMock.newProductRequest);
+    sinon.stub(ProductModel, 'create').resolves(mockCreateReturn);
 
+    const serviceResponse = await productService.create(parameters);
+
+    expect(serviceResponse).to.deep.eq(createMock.newProductResponse);
+  });
 });
---
+++ tests/mocks/create.mock.ts
@@ -0,0 +1,16 @@
+const newProductResponse = {
+  "id": 6,
+  "name": "Martelo de Thor",
+  "price": "30 peças de ouro"
+};
+
+const newProductRequest = {
+  "name": "Martelo de Thor",
+  "price": "30 peças de ouro",
+  "orderId": 4
+};
+
+export default {
+  newProductRequest,
+  newProductResponse,
+};
